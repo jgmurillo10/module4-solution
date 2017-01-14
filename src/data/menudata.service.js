5e36953d1c51b328f8910d074edf38c46099c9d6
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2017-01-13 22:52:18 -0500

    chhanges

--- src/data/menudata.service.js
+++ src/data/menudata.service.js
@@ -3,16 +3,26 @@
 	angular.module('data')
 	.service('MenuDataService', MenuDataService);
 
-
-	function MenuDataService () {
+	MenuDataService.$inject=['$http'];
+	function MenuDataService ($http) {
 		// body... 
 		service = this;
 
 		service.getAllCategories = function () {
+			return $http({
+				method: 'GET',
+				url: 'https://davids-restaurant.herokuapp.com/categories.json'
+			});
 			/* body... */
 		};
 		service.getItemsForCategory = function (categoryShortName) {
+			
+			return $http({
+				method: 'GET',
+				url: ('https://davids-restaurant.herokuapp.com/menu_items.json?category='+ categoryShortName)
+			});
 			/* body... */
+
 		};
 	};
 })();
